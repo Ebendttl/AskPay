57dec3caa1900043cbc8b75f6a93cebf3fa32c69
Ebendttl <ebendttl@gmail.com>
2026-07-11 23:37:27 +0100

    feat: wrap application layout with LanguageProvider for localization support

--- askpay-app/apps/web/src/app/layout.tsx
+++ askpay-app/apps/web/src/app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css';
 import { Navbar } from '@/components/navbar';
 import { SiteFooter } from '@/components/site-footer';
 import { WalletProvider } from "@/components/wallet-provider"
+import { LanguageProvider } from '@/hooks/useLanguage';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,15 +24,18 @@ export default function RootLayout({
       <body className={inter.className}>
         {/* Navbar is included on all pages */}
         <div className="relative flex min-h-screen flex-col">
-          <WalletProvider>
-            <Navbar />
-            <main className="flex-1">
-              {children}
-            </main>
-            <SiteFooter />
-          </WalletProvider>
+          <LanguageProvider>
+            <WalletProvider>
+              <Navbar />
+              <main className="flex-1">
+                {children}
+              </main>
+              <SiteFooter />
+            </WalletProvider>
+          </LanguageProvider>
         </div>
       </body>
     </html>
   );
 }
+
